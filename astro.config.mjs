76341a0b5410f7363cf7bc343890c692b2e58549
sanasuno <sanasunozzz@gmail.com>
2024-09-28 21:10:06 +0900

    エージェントページの追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,11 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://sanasuno.github.io',
+    vite: {
+      resolve: {
+        alias: {
+          '@images': path.resolve('./src/assets/images'),
+        },
+      },
+    },
 });
