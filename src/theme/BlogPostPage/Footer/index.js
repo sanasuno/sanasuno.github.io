ed3ddff3ba89f54f44bdfe6552cae1ba65e716c8
sanasuno <sanasunozzz@gmail.com>
2025-02-28 08:49:47 +0900

    コメント欄の設置

--- src/theme/BlogPostPage/Footer/index.js
+++ src/theme/BlogPostPage/Footer/index.js
@@ -1,6 +1,6 @@
-// src/theme/BlogPostItem/Footer/index.js
+// src/theme/BlogPostPage/Footer/index.js
 import React from 'react';
-import Footer from '@theme-original/BlogPostItem/Footer';
+import Footer from '@theme-original/BlogPostPage/Footer';
 import GiscusComments from '@site/src/components/GiscusComments';
 
 export default function FooterWrapper(props) {
